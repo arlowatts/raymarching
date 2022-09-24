4eab1a3f8366efc8364ab83be8ecc0c2c70e9cd6
Arlo <arlowatts1@gmail.com>
2022-09-23 21:10:59 -0700

    Remove redundant type conversions

--- src/shapes/Color.java
+++ src/shapes/Color.java
@@ -6,10 +6,10 @@ package src.shapes;
 public class Color {
 	public static final double RATIO = 1.0 / 255.0;
 	
-	// Methods to split the integer into its components
+	// Methods to split the color into its components
 	public static int getR(int color) {return (color >> 16) & 0xff;}
 	public static int getG(int color) {return (color >>  8) & 0xff;}
-	public static int getB(int color) {return (color      ) & 0xff;}
+	public static int getB(int color) {return  color        & 0xff;}
 	
 	// Methods to merge three components into a color
 	public static int toColor(int r, int g, int b) {return (r << 16) | (g << 8) | b;}
@@ -17,30 +17,30 @@ public class Color {
 	
 	// Shading a color by a value between 0 and 1
 	public static int shade(int color, double shade) {
-		return ((int)((double)getR(color) * shade) << 16) |
-			   ((int)((double)getG(color) * shade) << 8) |
-			   (int)((double)getB(color) * shade);
+		return ((int)(getR(color) * shade) << 16) |
+			   ((int)(getG(color) * shade) <<  8) |
+			    (int)(getB(color) * shade);
 	}
 	
 	// Shading the components of a color by values between 0 and 1
 	public static int shade(int color, double shadeR, double shadeG, double shadeB) {
-		return ((int)((double)getR(color) * shadeR) << 16) |
-			   ((int)((double)getG(color) * shadeG) << 8) |
-			   (int)((double)getB(color) * shadeB);
+		return ((int)(getR(color) * shadeR) << 16) |
+			   ((int)(getG(color) * shadeG) <<  8) |
+			    (int)(getB(color) * shadeB);
 	}
 	
 	// Shading the components of a color by corresponding components of a vector
 	public static int shade(int color, Vector v) {
-		return ((int)((double)getR(color) * v.getX()) << 16) |
-			   ((int)((double)getG(color) * v.getY()) << 8) |
-			   (int)((double)getB(color) * v.getX());
+		return ((int)(getR(color) * v.getX()) << 16) |
+			   ((int)(getG(color) * v.getY()) <<  8) |
+			    (int)(getB(color) * v.getX());
 	}
 	
 	// Shading a color by another color
 	public static int shade(int color, int shade) {
-		return ((int)((double)(getR(color) * getR(shade)) * RATIO) << 16) |
-			   ((int)((double)(getG(color) * getG(shade)) * RATIO) << 8) |
-			   (int)((double)(getB(color) * getB(shade)) * RATIO);
+		return ((int)(getR(color) * getR(shade) * RATIO) << 16) |
+			   ((int)(getG(color) * getG(shade) * RATIO) <<  8) |
+			    (int)(getB(color) * getB(shade) * RATIO);
 	}
 	
 	public static int invert(int color) {return 0xffffff - color;}
