a0b7c40c343c536b7f2d9932c8fa4207b151dabc
Arlo <arlowatts1@gmail.com>
2022-10-06 08:56:54 -0700

    Fix cone sdf and implement normal function

--- src/shapes/Cone.java
+++ src/shapes/Cone.java
@@ -20,29 +20,49 @@ public class Cone extends Shape {
 		v.subtract(getPos());
 		v.inverseRotate(getAngle());
 		
-		Vector q = new Vector(Math.sqrt(v.x*v.x + v.z*v.z), v.y, 0);
-		
-		Vector a = new Vector(radius, 0, 0);
-		Vector b = new Vector(-radius, height, 0);
-		b.setLength(1);
-		
-		double gamma = q.dotProduct(b) - a.dotProduct(b);
-		
-		b.multiply(gamma);
-		a.add(b);
-		a.y = Math.min(Math.max(a.y, 0), height);
-		
-		double distance = q.getDistance(a);
-		
-		if (q.y > 0 && q.y < height && q.x < radius * (height - q.y) / height) distance *= -1;
+		Vector q = new Vector(Math.sqrt(v.x*v.x + v.z*v.z), v.y + height / 2, 0);
 		
 		v.rotate(getAngle());
 		v.add(getPos());
+		
+		Vector a = new Vector(radius, 0, 0);
+		Vector b = new Vector(-radius, height, 0);
+		
+		double gamma = Math.min(Math.max((q.dotProduct(b) - a.dotProduct(b)) / b.dotProduct(b), 0), 1);
+		
+		b.multiply(gamma);
+		a.add(b);
+		
+		Vector c = new Vector(Math.min(q.x, radius), 0, 0);
+		
+		double distance = Math.min(q.getDistance(a), q.getDistance(c));
+		
+		if (q.y > 0 && q.y < height && q.x < radius * (height - q.y) / height) distance *= -1;
 		
 		return distance;
 	}
 	
 	protected void setBoundRadius() {
-		setBoundRadius(height);
+		setBoundRadius(Math.sqrt(radius*radius + height*height/4));
+	}
+	
+	public Vector getNormal(Vector v) {
+		Vector n = new Vector();
+		
+		v.subtract(getPos());
+		v.inverseRotate(getAngle());
+		
+		if (v.y < -height / 2) n.set(0, -1, 0);
+		else {
+			n.set(v.x, radius / height * Math.sqrt(v.x*v.x + v.z*v.z), v.z);
+			n.setLength(1);
+		}
+		
+		n.rotate(getAngle());
+		
+		v.rotate(getAngle());
+		v.add(getPos());
+		
+		return n;
 	}
 }
