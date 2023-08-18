e4a6a759824301aa264e20cbb2629f829d571935
Arlo <arlowatts1@gmail.com>
2023-08-17 22:40:29 -0700

    Add texture mapping for box

--- src/raymarching/shapes/Box.java
+++ src/raymarching/shapes/Box.java
@@ -33,6 +33,52 @@ public class Box extends Shape {
 		return q.getLength() + d;
 	}
 	
+	@Override
+	protected int getLocalColor(Vector r) {
+		if (getTexture() == null) return getColor();
+
+		int x = 0, y = 0;
+
+		Vector q = new Vector(r);
+		q.positive();
+
+		if (q.x > q.y && q.x > q.z) {
+			if (r.x < 0) {
+				x = (int)(Math.min(Math.max(-r.z / 2 + 0.5, 0), 1) * (getTexture().getWidth() - 1) / 6.0);
+				y = (int)(Math.min(Math.max(r.y / 2 + 0.5, 0), 1) * (getTexture().getHeight() - 1));
+			}
+			else {
+				x = (int)((Math.min(Math.max(r.z / 2 + 0.5, 0), 1) + 1) * (getTexture().getWidth() - 1) / 6.0);
+				y = (int)(Math.min(Math.max(r.y / 2 + 0.5, 0), 1) * (getTexture().getHeight() - 1));
+			}
+		}
+		else if (q.z > q.y && q.z > q.x) {
+			if (r.z < 0) {
+				x = (int)((Math.min(Math.max(-r.x / 2 + 0.5, 0), 1) + 2) * (getTexture().getWidth() - 1) / 6.0);
+				y = (int)(Math.min(Math.max(r.y / 2 + 0.5, 0), 1) * (getTexture().getHeight() - 1));
+			}
+			else {
+				x = (int)((Math.min(Math.max(r.x / 2 + 0.5, 0), 1) + 3) * (getTexture().getWidth() - 1) / 6.0);
+				y = (int)(Math.min(Math.max(r.y / 2 + 0.5, 0), 1) * (getTexture().getHeight() - 1));
+			}
+		}
+		else if (q.y > q.z && q.y > q.x) {
+			if (r.y < 0) {
+				x = (int)((Math.min(Math.max(-r.x / 2 + 0.5, 0), 1) + 4) * (getTexture().getWidth() - 1) / 6.0);
+				y = (int)(Math.min(Math.max(-r.z / 2 + 0.5, 0), 1) * (getTexture().getHeight() - 1));
+			}
+			else {
+				x = (int)((Math.min(Math.max(r.x / 2 + 0.5, 0), 1) + 5) * (getTexture().getWidth() - 1) / 6.0);
+				y = (int)(Math.min(Math.max(r.z / 2 + 0.5, 0), 1) * (getTexture().getHeight() - 1));
+			}
+		}
+		else {
+			return getColor();
+		}
+		
+		return getTexture().getRGB(x, y);
+	}
+	
 	protected double setBoundRadius() {
 		return Math.sqrt(3);
 	}
